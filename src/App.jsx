dc5650b0495da76c3dc7e885cc0db3c724f1b5fe
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-12-10 00:00:25 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import CalculateCircleArea from "./components/calculateCircleArea/CalculateCircleArea";
 import CalculateSquareArea from "./components/calculateSquareArea/CalculateSquareArea";
 import CalculateTriangleArea from "./components/calculateTriangleArea/CalculateTriangleArea";
-import CelsiusToFahrenheit from "./components/CelsiusToFahrenheit/CelsiusToFahrenheit";
+import CelsiusToFahrenheit from "./components/celsiusToFahrenheit/CelsiusToFahrenheit";
 import FahrenheitToCelsius from "./components/fahrenheitToCelsius/FahrenheitToCelsius";
 import SayHello from "./components/sayHello/SayHello";
 import TotalPrice from "./components/totalPrice/TotalPrice";
@@ -10,10 +10,6 @@ import WriteMessage from "./components/writeMessage/WriteMessage";
 const App = () => {
 	return (<> 
 	<SayHello>Xavier</SayHello>
-	<SayHello>Diego</SayHello>
-	<SayHello>Josefa</SayHello>
-	<SayHello>Macarena</SayHello>
-	<SayHello>Adrián</SayHello>
 	<CalculateSquareArea base={15} heigh={15}></CalculateSquareArea>
 	<CalculateTriangleArea base={20} heigh={20}></CalculateTriangleArea>
 	<CalculateCircleArea radius={13}></CalculateCircleArea>
--- src/components/sayHello/SayHello.jsx
+++ src/components/sayHello/SayHello.jsx
@@ -1,6 +1,6 @@
 const SayHello = (props) =>{
     // console.log(props.children)
-    return <h2>{props.children}</h2>
+    return <h2> Hola {props.children}</h2>
 
 }
 
